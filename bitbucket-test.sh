91e2c53e9ef7127b8c2d826ff02d80263cfc7087
Tim Aliev <timaliev@gmail.com>
2015-03-19 02:11:29 +0300

    Added REST API call to get JSON

--- bitbucket-test.sh
+++ bitbucket-test.sh
@@ -1,59 +1,74 @@
 #!/bin/bash
-exec scala -savecompiled -classpath "lib/lift-json_2.11-3.0-SNAPSHOT.jar:play-ws_2.11-2.3.8.jar:play_2.11-2.3.8.jar" "$0" "$0" "$@"
+exec scala -savecompiled -classpath "lib/lift-json_2.11-3.0-SNAPSHOT.jar:lib/commons-codec-1.9.jar:lib/httpclient-4.4.jar:lib/commons-logging-1.2.jar:lib/logback-core-1.1.2.jar:lib/slf4j-1.7.10/slf4j-api-1.7.10.jar:lib/slf4j-1.7.10/slf4j-simple-1.7.10.jar" "$0" "$0" "$@"
 !#
 
 import scala.io.Source
+import java.net.URL
 import sys.process._
+
+// For Lift JSON parser
 import net.liftweb.json.DefaultFormats
 import net.liftweb.json._
+import org.apache.commons.codec.binary.Base64;
 
+object HttpBasicAuth {
+   val BASIC = "Basic"
+   val AUTHORIZATION = "Authorization"
 
-// Say hello to the first argument 
+   def encodeCredentials(username: String, password: String): String = {
+      new String(Base64.encodeBase64String((username + ":" + password).getBytes))
+   }
+
+   def getHeader(username: String, password: String): String = 
+      BASIC + " " + encodeCredentials(username, password)
+}
 
 object PrintBibucketIssues {
 
   	def main(args: Array[String]) {
 
+		implicit val formats = DefaultFormats
+
 		val fileContent : String = args.length match {
-			case 1 => throw new IllegalArgumentException("No file name given. Use: " + args(0).toString + " [JSON file name]\n")
-			case 2 => Source.fromFile(args(1)).getLines.mkString("\n")
-			case _ => throw new IllegalArgumentException("Too many arguments. Use: " + args(0).toString + " [JSON file name]\n")
+			case 1 => { val bitbucketRepositorySPEditor = "semanticplatform/ontologyeditor/"
+						val bitbucketApiUrlv1 = "https://api.bitbucket.org/1.0/"
+						val bitbucketRepositoriesUrlv1 = bitbucketApiUrlv1 + "repositories/"
+						val bitbucketIssuesOptions = "limit=50&sort=local_id"
+						val bitbucketIssuesUrl = bitbucketRepositoriesUrlv1 + bitbucketRepositorySPEditor + "issues" + "?" + bitbucketIssuesOptions
+
+						println ("Reading JSON from URL: " + bitbucketIssuesUrl + " ...")
+						val username = readLine("Please, enter your BitBucket user name: ")
+						print("Please, enter your BitBucket password: ")
+						print(Console.INVISIBLE)
+						val password = readLine
+						print(Console.RESET)
+
+						val connection = new URL(bitbucketIssuesUrl).openConnection
+						connection.setRequestProperty(HttpBasicAuth.AUTHORIZATION, HttpBasicAuth.getHeader(username, password))
+						Source.fromInputStream(connection.getInputStream).getLines.mkString("\n")
+			}
+			case 2 => { println ("Reading JSON from File: " + args(1).toString + " ...")
+						Source.fromFile(args(1)).getLines.mkString("\n")
+			}
+			case _ => throw new IllegalArgumentException("Too many arguments.\n\n\tUse:\n\n\t\t" + args(0).toString + " [JSON file name] to read issues from file, or\n\n\t\t" + args(0).toString + " without arguments to read issues from BitBucket's RESTfull API\n")
 		}
 
-		implicit val formats = DefaultFormats // {
-			// override def dateFormatter = new SimpleDateFormat("yyyy-MM-dd")
-		// }
-		case class BBIssue(
-			assignee: Option[String],
-			component: Option[String],
-			content: Option[String],
-			content_updated_on: String,
-			created_on: String,
-			edited_on: Option[String],
-			id: Int,
-			kind: String,
-			milestone: Option[String],
-			priority: String,
-			reporter: Option[String],
-			status: String,
-			title: String,
-			updated_on: String,
-			version: Option[String],
-			watchers: List[String],
-			voters: List[String]
-		)
+		println (fileContent)
+		println ("\n----------\n")
 
-    	val issues = (parse(fileContent) \ "issues").children
+    	val issues = (parse(fileContent) \ "issues")
 
     	// val issuesList = issues.values
     	// val bbIssues = for(is <- issues) yield is.extract[BBIssue]
-	    val headers = "id,kind,status,priority,title,content,assignee,reporter,created_on,milestone,version,updated_on"
-	    println(headers)
+	    // val headers = "id,kind,status,priority,title,content,assignee,reporter,created_on,milestone,version,updated_on"
+	    // println(headers)
 
-	    val testJson = pretty(render(parse(fileContent) \ "local_id" ))
-	    println(s"\n ${testJson}\n")
+	    val testJson = pretty(render(issues))
+	    println(s"\n ${testJson}\n\n\n")
 
-		val javaProcs = ("ps auxw" #| "grep java").!!.trim
+		val procs = ("ps auxw" #| "grep java").!!.trim
+
+		println(procs)
 /*
     	for (i <- issues) {
 	    	val assignee = (i \ "assignee").extract[String] match {
@@ -107,3 +122,4 @@ object PrintBibucketIssues {
 }
 
 PrintBibucketIssues.main(args)
+
